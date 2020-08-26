a5ee2c674259bb502b1134ce4763b9984506bc8b
Gevorg Davoian <gdavoian@gmail.com>
2020-08-26 19:49:15 +0300

    Fix Flake8

--- bundlebuilder/models/__init__.py
+++ bundlebuilder/models/__init__.py
@@ -1,3 +1,11 @@
 # Make the classes below importable from the `.models` sub-package directly.
-from .primary import *
-from .secondary import *
+from .primary import (
+    Bundle,
+    Indicator,
+    Judgement,
+    Relationship,
+    Sighting,
+)
+from .secondary import (
+    Verdict,
+)
